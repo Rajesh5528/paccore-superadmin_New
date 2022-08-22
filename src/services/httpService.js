0c1a7281b051cf6fcbf40384547a1f1ab605592d
Rajesh5528 <rajesh.v@paccore.com>
2022-08-22 18:29:49 +0530

    changes

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+
 
 // axios.defaults.baseURL = 'https://dev.bijbol.com/api/v6/';
 axios.defaults.baseURL = 'http://15.207.163.215:8000/api/v6/';
 
-
 axios.interceptors.response.use(null, (error) => {
+	// let navigate = useNavigate();
+
 	const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
 	if (!expectedError) {
 		console.log(expectedError,'expectedError');
@@ -14,7 +17,8 @@ axios.interceptors.response.use(null, (error) => {
 			position: toast.POSITION.BOTTOM_LEFT
 		  });
 		  setTimeout(() => {
-			window.location =('/login')
+			window.location.reload(false)
+			// navigate("/login", { replace: true });
 		  }, 2000);
 		  
 		// alert('No Network, Please Connect to Internet');
